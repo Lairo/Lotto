0d2e534b0a248ae67d16ed2e118014a5ec0579dd
Lairo <chajus1@googlemail.com>
2018-09-21 16:31:14 +0100

    organising files

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+ 1-59 numbers
+
+ The player selects 6 individual numbers from the range 1-59.
+6 Balls are then drawn randomly from the 59 balls available.
+Prizes are awarded for matching 3, 4, 5 and 6 Ball numbers.
+3 = 50
+4 = 100
+5 = 200
+6 = 500
+
+*/
+
+
+
